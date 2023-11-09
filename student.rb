0e2470d38004724e9b8868e7af63a0778b52e0f0
Titus kiplagat <tituskiplagat272@gmail.com>
2023-11-09 19:49:33 +0300

    Revert "OOP school library: decorate a class"

--- student.rb
+++ student.rb
@@ -1,8 +1,6 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
-
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
@@ -10,10 +8,5 @@ class Student < Person
 
   def play_hooky
     '¯(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students << self unless classroom.students.include?(self)
   end
 end
