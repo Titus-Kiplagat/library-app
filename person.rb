7f3acfdb6e1d80b76b0273d9ff2d766b6f37f66a
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-11-11 14:28:36 +0300

    Add missing files

--- person.rb
+++ person.rb
@@ -1,12 +1,19 @@
-class Person
-  attr_reader :id
+require_relative 'nameable'
+require_relative 'trimmer_decorator'
+require_relative 'capitalize_decorator'
+require 'securerandom'
+
+class Person < Nameable
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name, parent_permission: true)
+    super()
     @id = SecureRandom.uuid
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -16,4 +23,21 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts "Original Name: #{person.correct_name}"
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts "Capitalized Name: #{capitalized_person.correct_name}"
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
